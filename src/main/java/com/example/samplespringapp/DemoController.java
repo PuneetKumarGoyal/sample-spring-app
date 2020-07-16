e9bb4a12294940a4d0e99bdf2965a07b591a62d0
PuneetKumarGoyal <PuneetKumarGoyal@gmail.com>
2020-07-16 14:37:38 +0200

    first commit

---
+++ src/main/java/com/example/samplespringapp/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.samplespringapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/sayhello")
+    public String sayHello(){
+        return "saying hello from sample spring boot app";
+    }
+}
